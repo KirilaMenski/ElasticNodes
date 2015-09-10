2e1aadc97f6810586085ba3ce43b3585db7c9ec3
Mensky <bv_1514@mail.ru>
2015-09-10 15:44:50 +0300

    Added new key

--- ElasticNodes/src/by/ansgar/nodes/input/KeyInput.java
+++ ElasticNodes/src/by/ansgar/nodes/input/KeyInput.java
@@ -3,35 +3,31 @@ package by.ansgar.nodes.input;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import by.ansgar.nodes.entity.Cells;
+import by.ansgar.nodes.scene.Scene;
 
 public class KeyInput implements KeyListener {
 
-	@Override
 	public void keyPressed(KeyEvent e) {
-	int key = e.getKeyCode();
-		
-		if(key == KeyEvent.VK_ESCAPE){
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_ESCAPE) {
 			System.exit(1);
 		}
-		if(key == KeyEvent.VK_S){
+		if (key == KeyEvent.VK_R) {
+			Scene.cells.get(0).setX(Scene.cells.get(0).getX() - 100);
+		}
+		if (key == KeyEvent.VK_S) {
 			System.out.println("Hello!");
 		}
-		
+
 	}
 
-	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	
+	}
 
 }
