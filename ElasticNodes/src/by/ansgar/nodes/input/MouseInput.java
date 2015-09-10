e917cd8c135259d5496044c51baede250ca78ec4
Mensky <bv_1514@mail.ru>
2015-09-10 15:45:26 +0300

    Modified

--- ElasticNodes/src/by/ansgar/nodes/input/MouseInput.java
+++ ElasticNodes/src/by/ansgar/nodes/input/MouseInput.java
@@ -3,8 +3,6 @@ package by.ansgar.nodes.input;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 import by.ansgar.nodes.entity.Cells;
 import by.ansgar.nodes.scene.Scene;
@@ -18,47 +16,34 @@ public class MouseInput implements MouseListener, MouseMotionListener{
 	public static boolean mousePressed = false;
 	
 
-	@Override
 	public void mouseDragged(MouseEvent e) {
 		mouseDX = e.getX();
 		mouseDY = e.getY();
-//		System.out.println("MouseDX = " + mouseDX + " | MouseDY = " + mouseDY);
 	}
 
-	@Override
 	public void mouseMoved(MouseEvent e) {
-//		mouseX = e.getX();
-//		mouseY = e.getY();
-//		System.out.println("MouseDX = " + mouseDX + " | MouseDY = " + mouseDY);
+
 	}
 
-	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void mousePressed(MouseEvent e) {
 		mouseX = e.getX();
 		mouseY = e.getY();
 		mousePressed = true;
 		
 	}
-
-	@Override
+	
 	public void mouseReleased(MouseEvent e) {
 		mousePressed = false;
 		
